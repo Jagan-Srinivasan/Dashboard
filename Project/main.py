529514ae9c424dec690f467d0dc93d4fe052375f
JAGAN S <jaganrmkcet@gmail.com>
2025-05-27 21:56:03 +0530

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -1,10 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello, World!"
 
 from flask import Flask, request, render_template_string, jsonify
 import time
